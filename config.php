a3f6ce7807420183a0f15e96f9f99282d1212ed5
zechariah123 <zechariahzparker@gmail.com>
2026-07-02 21:29:13 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $api_host = "c30c069656070f.880008df.nip.io";
 $api_token = "5d4e4579e4ca951cdfbea106d0f8c0";
-$api_prefix = "/gyf78erg";
+$api_prefix = "gyf78erg";
 
 $telegram_bot_token = "8928618766:AAFTowV26o-3B6Ft2Yr_9wmL0EWQ11RRf_4";
 $telegram_chat_id = "7121346338";
